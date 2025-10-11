33250292a023b5aece8b3f2183804e8d2bea0ad5
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-11 19:49:41 +0100

    Testing our value objects

---
+++ CleanTeeth.Test/Domain/ValueObjects/EmailTests.cs
@@ -0,0 +1,32 @@
+﻿using CleanTeeth.Domain.Exceptions;
+using CleanTeeth.Domain.ValueObjects;
+
+namespace CleanTeeth.Test.Domain.ValueObjects;
+
+[TestClass]
+public class EmailTests
+{
+    [TestMethod]
+    public void Constructor_NullEmail_ThrowsBusinessRuleException()
+    {
+        // Act & Assert
+        Assert.Throws<BusinessRuleException>(() => new Email(null!));
+    }
+
+    [TestMethod]
+    public void Constructor_EmailWithoutAtSymbol_ThrowsBusinessRuleException()
+    {
+        // Act & Assert
+        Assert.Throws<BusinessRuleException>(() => new Email("felipe.com"));
+    }
+
+    [TestMethod]
+    public void Constructor_ValidEmail_NoExceptions()
+    {
+        string validEmail = "felipe@example.com";
+
+        Email email = new(validEmail);
+
+        Assert.AreEqual(validEmail, email.Value);
+    }
+}
---
+++ CleanTeeth.Test/Domain/ValueObjects/TimeIntervalTests.cs
@@ -0,0 +1,31 @@
+﻿using CleanTeeth.Domain.Exceptions;
+using CleanTeeth.Domain.ValueObjects;
+
+namespace CleanTeeth.Test.Domain.ValueObjects;
+
+[TestClass]
+public class TimeIntervalTests
+{
+    [TestMethod]
+    public void Constructor_StartIsAfterEnd_ThrowsBusinessRuleException()
+    {
+        // Arrange
+        DateTimeOffset start = DateTimeOffset.UtcNow;
+        DateTimeOffset end = DateTimeOffset.UtcNow.AddHours(-1);
+        // Act & Assert
+        Assert.Throws<BusinessRuleException>(() => new TimeInterval(start, end));
+    }
+
+    [TestMethod]
+    public void Constructor_ValidTimeInterval_NoExceptions()
+    {
+        // Arrange
+        DateTimeOffset start = DateTimeOffset.UtcNow;
+        DateTimeOffset end = DateTimeOffset.UtcNow.AddHours(1);
+        // Act
+        TimeInterval timeInterval = new(start, end);
+        // Assert
+        Assert.AreEqual(start, timeInterval.Start);
+        Assert.AreEqual(end, timeInterval.End);
+    }
+}
