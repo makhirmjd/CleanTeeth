03a8cf8cea853176c046b9564c433d754851a44f
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-11 12:04:45 +0100

    Anemic domain entities

--- CleanTeeth.Domain/Entities/DentalOffice.cs
+++ CleanTeeth.Domain/Entities/DentalOffice.cs
@@ -4,4 +4,14 @@ public class DentalOffice
 {
     public Guid Id { get; private set; }
     public string Name { get; private set; } = default!;
+
+    public DentalOffice(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new Exceptions.BusinessRuleException($"The {nameof(name)} is required");
+        }
+        Id = Guid.CreateVersion7();
+        Name = name;
+    }
 }
