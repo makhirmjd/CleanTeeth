04c00c60b12f17e6ca907320965ee7e64f37bad8
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-12 07:25:17 +0100

    Testing our entities

---
+++ CleanTeeth.Test/Domain/Entities/AppointmentTests.cs
@@ -0,0 +1,94 @@
+﻿using CleanTeeth.Domain.Entities;
+using CleanTeeth.Domain.Entities.Enums;
+using CleanTeeth.Domain.Exceptions;
+using CleanTeeth.Domain.ValueObjects;
+
+namespace CleanTeeth.Test.Domain.Entities;
+
+[TestClass]
+public class AppointmentTests
+{
+    readonly Guid patientId = Guid.NewGuid();
+    readonly Guid dentistId = Guid.NewGuid();
+    readonly Guid dentalOfficeId = Guid.NewGuid();
+    readonly TimeInterval timeInterval = new(DateTimeOffset.UtcNow.AddHours(1), DateTimeOffset.UtcNow.AddHours(2));
+
+
+    [TestMethod]
+    public void Constructor_NullTimeInterval_ThrowsBusinessRuleException()
+    {
+        TimeInterval timeInterval = null!;
+        // Act & Assert
+        BusinessRuleException exception = Assert.Throws<BusinessRuleException>(() =>
+            new Appointment(patientId, dentistId, dentalOfficeId, timeInterval));
+        Assert.AreEqual("The time interval is required", exception.Message);
+    }
+
+    [TestMethod]
+    public void Constructor_StartTimeInThePast_ThrowsBusinessRuleException()
+    {
+        TimeInterval timeInterval = new(DateTimeOffset.UtcNow.AddHours(-1), DateTimeOffset.UtcNow.AddHours(1));
+        // Act & Assert
+        BusinessRuleException exception = Assert.Throws<BusinessRuleException>(() =>
+            new Appointment(patientId, dentistId, dentalOfficeId, timeInterval));
+        Assert.AreEqual("The start time cannot be in the past", exception.Message);
+    }
+
+    [TestMethod]
+    public void Constructor_ValidParameters_CreatesAppointment()
+    {
+        // Act
+        Appointment appointment = new(patientId, dentistId, dentalOfficeId, timeInterval);
+        // Assert
+        Assert.AreEqual(patientId, appointment.PatientId);
+        Assert.AreEqual(dentistId, appointment.DentistId);
+        Assert.AreEqual(dentalOfficeId, appointment.DentalOfficeId);
+        Assert.AreEqual(timeInterval, appointment.TimeInterval);
+        Assert.AreEqual(AppointmentStatus.Scheduled, appointment.Status);
+        Assert.AreNotEqual(Guid.Empty, appointment.Id);
+    }
+
+    [TestMethod]
+    public void Cancel_StatusNotScheduled_ThrowsBusinessRuleException()
+    {
+        // Arrange
+        Appointment appointment = new(patientId, dentistId, dentalOfficeId, timeInterval);
+        appointment.Complete();
+        // Act & Assert
+        BusinessRuleException exception = Assert.Throws<BusinessRuleException>(() => appointment.Cancel());
+        Assert.AreEqual("Only scheduled appointments can be cancelled", exception.Message);
+    }
+
+    [TestMethod]
+    public void Cancel_StatusScheduled_ChangesStatusToCancelled()
+    {
+        // Arrange
+        Appointment appointment = new(patientId, dentistId, dentalOfficeId, timeInterval);
+        // Act
+        appointment.Cancel();
+        // Assert
+        Assert.AreEqual(AppointmentStatus.Cancelled, appointment.Status);
+    }
+
+    [TestMethod]
+    public void Complete_StatusNotScheduled_ThrowsBusinessRuleException()
+    {
+        // Arrange
+        Appointment appointment = new(patientId, dentistId, dentalOfficeId, timeInterval);
+        appointment.Cancel();
+        // Act & Assert
+        BusinessRuleException exception = Assert.Throws<BusinessRuleException>(() => appointment.Complete());
+        Assert.AreEqual("Only scheduled appointments can be completed", exception.Message);
+    }
+
+    [TestMethod]
+    public void Complete_StatusScheduled_ChangesStatusToCompleted()
+    {
+        // Arrange
+        Appointment appointment = new(patientId, dentistId, dentalOfficeId, timeInterval);
+        // Act
+        appointment.Complete();
+        // Assert
+        Assert.AreEqual(AppointmentStatus.Completed, appointment.Status);
+    }
+}
---
+++ CleanTeeth.Test/Domain/Entities/DentistTests.cs
@@ -0,0 +1,35 @@
+﻿using CleanTeeth.Domain.Entities;
+using CleanTeeth.Domain.Exceptions;
+using CleanTeeth.Domain.ValueObjects;
+
+namespace CleanTeeth.Test.Domain.Entities;
+
+[TestClass]
+public class DentistTests
+{
+    [TestMethod]
+    public void Constructor_NullName_ThrowsBusinessRuleException()
+    {
+        Assert.Throws<BusinessRuleException>(() => new Dentist(name: default!, new Email("felipe@example.com")));
+    }
+
+    [TestMethod]
+    public void Constructor_NullEmail_ThrowsBusinessRuleException()
+    {
+        Assert.Throws<BusinessRuleException>(() => new Dentist("Samson Viper", email: default!));
+    }
+
+    [TestMethod]
+    public void Constructor_ValidParameters_CreatesDentist()
+    {
+        // Arrange
+        var name = "Samson Viper";
+        var email = new Email("samson.viper@example.com");
+        // Act
+        Dentist dentist = new(name, email);
+        // Assert
+        Assert.AreNotEqual(Guid.Empty, dentist.Id);
+        Assert.AreEqual(name, dentist.Name);
+        Assert.AreEqual(email, dentist.Email);
+    }
+}
