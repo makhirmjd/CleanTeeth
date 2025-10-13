facbcdc86b6cd760be8b37625a5afaf25009384f
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-13 09:25:05 +0100

    Validation exception

---
+++ CleanTeath.Application/Exceptions/CustomValidationException.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation.Results;
+
+namespace CleanTeath.Application.Exceptions;
+
+public class CustomValidationException : Exception
+{
+    public List<string> ValidationErrors { get; set; } = [];
+
+    public CustomValidationException(ValidationResult validationResult)
+    {
+        ValidationErrors.AddRange(validationResult.Errors.Select(x => x.ErrorMessage));
+    }
+}
