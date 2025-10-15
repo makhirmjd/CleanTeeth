d24d2e99cbadc26091b5f281ee1394d7afe4d101
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-15 15:20:30 +0100

    Mapping our dto

---
+++ CleanTeath.Application/Features/DentalOffices/Queries/GetDentalOfficeDetail/MapperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CleanTeeth.Domain.Entities;
+
+namespace CleanTeath.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+
+public static class MapperExtensions
+{
+    public static DentalOfficeDetailDto ToDto(this DentalOffice dentalOffice) =>
+        new()
+        {
+            Id = dentalOffice.Id,
+            Name = dentalOffice.Name
+        };
+}
