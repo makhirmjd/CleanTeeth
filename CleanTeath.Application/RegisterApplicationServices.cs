7a3085f9d20a52b2ac4ce6164733ca940385cf3f
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-15 15:36:32 +0100

    Registering the application layer service

---
+++ CleanTeath.Application/RegisterApplicationServices.cs
@@ -0,0 +1,17 @@
+﻿using CleanTeath.Application.Features.DentalOffices.Commands.CreateDentalOffice;
+using CleanTeath.Application.Features.DentalOffices.Queries.GetDentalOfficeDetail;
+using CleanTeath.Application.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanTeath.Application;
+
+public static class RegisterApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services) 
+    {
+        services.AddTransient<IMediator, SimpleMediator>();
+        services.AddScoped<IRequestHandler<CreateDentalOfficeCommand, Guid>, CreateDentalOfficeCommandHandler>();
+        services.AddScoped<IRequestHandler<GetDentalOfficeDetailQuery, DentalOfficeDetailDto>, GetDentalOfficeDetailQueryHandler>();
+        return services;
+    }
+}
