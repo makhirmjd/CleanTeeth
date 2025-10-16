8a90beb18a08018403579b74ce2f5248707ef55a
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-16 08:17:11 +0100

    Installing entity framework core

---
+++ CleanTeeth.Persistence/CleanTeethDbContext.cs
@@ -0,0 +1,9 @@
+﻿using CleanTeeth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanTeeth.Persistence;
+
+public class CleanTeethDbContext(DbContextOptions<CleanTeethDbContext> options) : DbContext(options)
+{
+    public DbSet<DentalOffice> DentalOffices => Set<DentalOffice>();
+}
