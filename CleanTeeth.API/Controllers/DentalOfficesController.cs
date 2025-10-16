8bae883ad60db16e63b7e435d393b4dfd0e5eb2c
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-16 15:08:59 +0100

    Creating our first controller

---
+++ CleanTeeth.API/Controllers/DentalOfficesController.cs
@@ -0,0 +1,19 @@
+﻿using CleanTeath.Application.Features.DentalOffices.Commands.CreateDentalOffice;
+using CleanTeath.Application.Utilities;
+using CleanTeeth.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanTeeth.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DentalOfficesController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateDentalOfficeDto createDentalOfficeDto)
+    {
+        var command = new CreateDentalOfficeCommand { Name = createDentalOfficeDto.Name};
+        Guid id = await mediator.Send(command);
+        return Ok(id);
+    }
+}
