aeca4be8f701c1c806b37eaab0e60e4c78b4a2c7
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-16 09:55:33 +0100

    Implementing the unit of work

---
+++ CleanTeeth.Persistence/UnitsOfWork/EfCoreUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using CleanTeath.Application.Contracts.Persistence;
+
+namespace CleanTeeth.Persistence.UnitsOfWork;
+
+public class EfCoreUnitOfWork(CleanTeethDbContext context) : IUnitOfWork
+{
+    public async Task Commit() => await context.SaveChangesAsync();
+
+    public Task Rollback() => Task.CompletedTask;
+}
