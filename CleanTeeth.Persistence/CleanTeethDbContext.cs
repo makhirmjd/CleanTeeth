4375db92c045aff383176550858021d8da9e3bc7
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-16 09:16:35 +0100

    Configuring an entity

--- CleanTeeth.Persistence/CleanTeethDbContext.cs
+++ CleanTeeth.Persistence/CleanTeethDbContext.cs
@@ -6,4 +6,11 @@ namespace CleanTeeth.Persistence;
 public class CleanTeethDbContext(DbContextOptions<CleanTeethDbContext> options) : DbContext(options)
 {
     public DbSet<DentalOffice> DentalOffices => Set<DentalOffice>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly((typeof(CleanTeethDbContext)).Assembly);
+    }
 }
---
+++ CleanTeeth.Persistence/Configurations/DentalOfficeConfig.cs
@@ -0,0 +1,15 @@
+﻿using CleanTeeth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanTeeth.Persistence.Configurations;
+
+public class DentalOfficeConfig : IEntityTypeConfiguration<DentalOffice>
+{
+    public void Configure(EntityTypeBuilder<DentalOffice> builder)
+    {
+        builder.Property(d => d.Name)
+            .HasMaxLength(150)
+            .IsRequired();
+    }
+}
