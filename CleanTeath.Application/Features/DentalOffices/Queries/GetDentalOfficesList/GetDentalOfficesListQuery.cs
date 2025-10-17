64184d321833c4f0135c080e11add3efa2cbce5c
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-17 09:49:53 +0100

    Getting the list of dental offices

---
+++ CleanTeath.Application/Features/DentalOffices/Queries/GetDentalOfficesList/GetDentalOfficesListQuery.cs
@@ -0,0 +1,5 @@
+﻿using CleanTeath.Application.Utilities;
+
+namespace CleanTeath.Application.Features.DentalOffices.Queries.GetDentalOfficesList;
+
+public class GetDentalOfficesListQuery : IRequest<List<DentalOfficesListDto>> {}
