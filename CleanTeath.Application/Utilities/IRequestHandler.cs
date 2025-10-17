f379f14672c3457346277f9cfc60a258ed522476
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-17 20:50:33 +0100

    Refactor our mediator

--- CleanTeath.Application/Utilities/IRequestHandler.cs
+++ CleanTeath.Application/Utilities/IRequestHandler.cs
@@ -4,3 +4,8 @@ public interface IRequestHandler<TRequest, TResponse>
 {
     Task<TResponse> Handle(TRequest request);
 }
+
+public interface IRequestHandler<TRequest>
+{
+    Task Handle(TRequest request);
+}
