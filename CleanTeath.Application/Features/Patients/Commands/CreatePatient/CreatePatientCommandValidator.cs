b4c9b350fa5b3726fa874792cb3a8099be4f74f8
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-20 10:31:41 +0100

    Create Patient

---
+++ CleanTeath.Application/Features/Patients/Commands/CreatePatient/CreatePatientCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace CleanTeath.Application.Features.Patients.Commands.CreatePatient;
+
+public class CreatePatientCommandValidator : AbstractValidator<CreatePatientCommand>
+{
+    public CreatePatientCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("The field {PropertyName} is required");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("The field {PropertyName} is required")
+            .EmailAddress().WithMessage("Invalid email format");
+    }
+}
