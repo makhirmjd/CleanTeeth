518669d5502d912af03e1436ec410c88635086b1
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-21 14:49:30 +0100

    Checkpoint

---
+++ CleanTeeth.Test/Application/Features/Patients/GetPatientListQueryHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using CleanTeath.Application.Contracts.Repositories;
+using CleanTeath.Application.Features.Patients.Queries.GetPatientList;
+using CleanTeeth.Domain.Entities;
+using CleanTeeth.Domain.ValueObjects;
+using NSubstitute;
+
+namespace CleanTeeth.Tests.Application.Features.Patients;
+
+[TestClass]
+public class GetPatientListQueryHandlerTests
+{
+    private IPatientRepository repository = default!;
+    private GetPatientsListQueryHandler handler = default!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        repository = Substitute.For<IPatientRepository>();
+        handler = new GetPatientsListQueryHandler(repository);
+    }
+
+    [TestMethod]
+    public async Task Handle_ValidRequest_ReturnsPatientList()
+    {
+        var query = new GetPatientsListQuery { Page = 1, RecordsPerPage = 2 };
+        IEnumerable<Patient> patients = new Faker<Patient>()
+            .CustomInstantiator(f =>
+            {
+                string name = f.Name.FullName();
+                Email email = new(f.Internet.Email(name));
+                return new(name, email);
+            }).Generate(10);
+    }
+}
