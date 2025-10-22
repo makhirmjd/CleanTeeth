db147438cc718be9d4fcf4b2a95f8f4d34e7670b
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-22 17:43:15 +0100

    Get patient by id

---
+++ CleanTeath.Application/Features/Patients/Queries/GetPatientDetail/GetPatientDetailQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using CleanTeath.Application.Contracts.Repositories;
+using CleanTeath.Application.Exceptions;
+using CleanTeath.Application.Utilities.Mediator;
+using CleanTeeth.Domain.Entities;
+
+namespace CleanTeath.Application.Features.Patients.Queries.GetPatientDetail;
+
+public class GetPatientDetailQueryHandler(IPatientRepository repository) : IRequestHandler<GetPatientDetailQuery, PatientDetailDto>
+{
+    public async Task<PatientDetailDto> Handle(GetPatientDetailQuery request)
+    {
+        Patient patient = await repository.GetById(request.Id) ?? throw new NotFoundException();
+        return patient.ToDto();
+    }
+}
---
+++ CleanTeath.Application/Features/Patients/Queries/GetPatientDetail/MapperExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CleanTeeth.Domain.Entities;
+
+namespace CleanTeath.Application.Features.Patients.Queries.GetPatientDetail;
+
+public static partial class MapperExtensions
+{
+    public static PatientDetailDto ToDto(this Patient patient) =>
+        new()
+        {
+            Id = patient.Id,
+            Name = patient.Name,
+            Email = patient.Email.Value
+        };
+}
