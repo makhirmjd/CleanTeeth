2d1594b06fe23dd10469d9d0ac8fa4d03011c59d
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-23 09:06:19 +0100

    Delete patient

---
+++ CleanTeath.Application/Features/Patients/Commands/DeletePatient/DeletePatientCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CleanTeath.Application.Contracts.Persistence;
+using CleanTeath.Application.Contracts.Repositories;
+using CleanTeath.Application.Exceptions;
+using CleanTeath.Application.Utilities.Mediator;
+using CleanTeeth.Domain.Entities;
+
+namespace CleanTeath.Application.Features.Patients.Commands.DeletePatient;
+
+public class DeletePatientCommandHandler(IPatientRepository repository, IUnitOfWork unitOfWork) : IRequestHandler<DeletePatientCommand>
+{
+    public async Task Handle(DeletePatientCommand request)
+    {
+        Patient patient = await repository.GetById(request.Id) ?? throw new NotFoundException();
+
+        try
+        {
+            await repository.Delete(patient);
+            await unitOfWork.Commit();
+        }
+        catch
+        {
+            await unitOfWork.Rollback();
+            throw;
+        }
+    }
+}
