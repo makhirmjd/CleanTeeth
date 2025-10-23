bf64bc78e7b867c85c7f44e16c3c8e00c3a242b7
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-23 07:57:03 +0100

    Filtering patients

--- CleanTeath.Application/Features/Patients/Queries/GetPatientList/PatientsFilterDto.cs
+++ CleanTeath.Application/Features/Patients/Queries/GetPatientList/PatientsFilterDto.cs
@@ -4,4 +4,6 @@ public class PatientsFilterDto
 {
     public int Page { get; set; } = 1;
     public int RecordsPerPage { get; set; } = 10;
+    public string? Name { get; set; }
+    public string? Email { get; set; }
 }
--- CleanTeeth.Persistence/Utilities/IQuaryableExtensions.cs
+++ CleanTeeth.Persistence/Utilities/IQuaryableExtensions.cs
@@ -1,7 +1,12 @@
-﻿namespace CleanTeeth.Persistence.Utilities;
+﻿using System.Linq.Expressions;
+
+namespace CleanTeeth.Persistence.Utilities;
 
 internal static class IQuaryableExtensions
 {
     internal static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, int pageNumber, int pageSize) =>
         queryable.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+
+    internal static IQueryable<T> Filter<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> expression) => 
+        queryable.Where(expression);
 }
