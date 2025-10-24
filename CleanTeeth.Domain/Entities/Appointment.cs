efb289d3e0354b04a31ca7f8cf38a72d2f9bffcb
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-24 06:27:23 +0100

    Appointments table

--- CleanTeeth.Domain/Entities/Appointment.cs
+++ CleanTeeth.Domain/Entities/Appointment.cs
@@ -10,10 +10,12 @@ public class Appointment
     public Guid DentistId { get; private set; }
     public Guid DentalOfficeId { get; private set; }
     public AppointmentStatus Status { get; private set; }
-    public TimeInterval TimeInterval { get; private set; }
+    public TimeInterval TimeInterval { get; private set; } = default!;
     public Patient? Patient { get; private set; }
     public Dentist? Dentist { get; private set; }
     public DentalOffice? DentalOffice { get; private set; }
+
+    private Appointment() { }
 
     public Appointment(Guid patientId, Guid dentistId, Guid dentalOfficeId, TimeInterval timeInterval)
     {
