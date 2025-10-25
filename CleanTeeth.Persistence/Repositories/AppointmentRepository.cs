40d61deda71c9824efc2879ec4b44800b278b303
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-25 21:52:51 +0100

    Creating appointments

--- CleanTeeth.Persistence/Repositories/AppointmentRepository.cs
+++ CleanTeeth.Persistence/Repositories/AppointmentRepository.cs
@@ -1,8 +1,17 @@
 ﻿using CleanTeath.Application.Contracts.Repositories;
 using CleanTeeth.Domain.Entities;
+using CleanTeeth.Domain.Entities.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanTeeth.Persistence.Repositories;
 
 public class AppointmentRepository(CleanTeethDbContext context) : Repository<Appointment>(context), IAppointmentRepository
 {
+    private readonly CleanTeethDbContext context = context;
+    public async Task<bool> OverlapExists(Guid dentistId, DateTimeOffset start, DateTimeOffset end)
+    {
+        return await context.Appointments.AnyAsync(a =>
+            a.DentistId == dentistId && a.Status == AppointmentStatus.Scheduled && 
+            start < a.TimeInterval.End && end > a.TimeInterval.Start);
+    }
 }
