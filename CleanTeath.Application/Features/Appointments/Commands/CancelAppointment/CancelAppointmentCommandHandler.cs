a788154439873e4ebad24c950a13c16a6447edb3
Abdulmalik Muhammad <makhirmjd@gmail.com>
2025-10-27 13:49:25 +0100

    Canceling an appointment

---
+++ CleanTeath.Application/Features/Appointments/Commands/CancelAppointment/CancelAppointmentCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CleanTeath.Application.Contracts.Persistence;
+using CleanTeath.Application.Contracts.Repositories;
+using CleanTeath.Application.Exceptions;
+using CleanTeath.Application.Utilities.Mediator;
+using CleanTeeth.Domain.Entities;
+
+namespace CleanTeath.Application.Features.Appointments.Commands.CancelAppointment;
+
+public class CancelAppointmentCommandHandler(IAppointmentRepository repository, IUnitOfWork unitOfWork) : IRequestHandler<CancelAppointmentCommand>
+{
+    public async Task Handle(CancelAppointmentCommand request)
+    {
+        Appointment appointment = await repository.GetById(request.Id) ?? throw new NotFoundException();
+        appointment.Cancel();
+
+		try
+		{
+			await repository.Update(appointment);
+			await unitOfWork.Commit();
+        }
+		catch
+		{
+			await unitOfWork.Rollback();
+            throw;
+		}
+    }
+}
